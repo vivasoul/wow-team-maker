acd1af4c67c9c713b14993e52d40c0ee813b16c1
KiTae Lee <vivaoul@naver.com>
2022-02-05 23:07:55 +0900

    beta-1.0 commit

---
+++ js/common.js
@@ -0,0 +1,217 @@
+var INSTACNE_IMG_PREFIX = "img/instacnes/";
+var TALENT_IMG_PREFIX 	= "img/talents/";
+var COVENANT_IMG_PREFIX = "img/covenants/";
+
+var WowSpec = function(spec, cov) {
+	const _spec_role = spec.role;
+	const _spec_icon = spec.image;
+	const _spec_text = spec.name;
+	const _spec_bg = spec.portrait;
+	const _cov_icon = cov.image;
+	const _cov_text = cov.name;
+	const _id = spec.id +"-"+cov.id;
+
+	this.getSpecRole = function() { return _spec_role };
+	this.getSpecIcon = function() { return TALENT_IMG_PREFIX+_spec_icon };
+	this.getSpecText = function() { return _spec_text };
+	this.getSpecBg = function() { return TALENT_IMG_PREFIX+_spec_bg };
+	this.getCovIcon = function() { return COVENANT_IMG_PREFIX+_cov_icon };
+	this.getCovText = function() { return _cov_text };
+	this.getId = function() { return _id };
+};
+
+var ban_data = null;
+var createData = function(covs, clss){
+	var phead = [];
+	var head = [];
+	var body = {};
+
+	/* 직업 */
+	for(var id in clss) {
+		var c = clss[id];
+		var specz = c.specializations;
+		var job_clazz = c.clazz
+		var phead_clazz_repo = [job_clazz, job_clazz].map(e => "."+e.replace(/\s/g,"."));
+
+		/* 전문화 */
+		for(var i=0; i<specz.length; i++) {
+			var s = specz[i];
+			var sid = id+"-"+i;
+			var spec_clazz = job_clazz+" "+s.clazz;
+			var head_clazz_repo = [job_clazz, spec_clazz].map(e => "."+e.replace(/\s/g,"."));
+
+			/* 성약단 */
+			for(var cv in covs) {
+				var csid = sid+"-"+cv;
+				var cov_clazz = covs[cv]["clazz"]+" "+spec_clazz;
+				var cov_clazz_repo = [job_clazz, spec_clazz, covs[cv]["clazz"]].map(e => "."+e.replace(/\s/g,"."));
+				/* spec & cov */
+				body[csid] = {sid: sid, cid: cv, team: "", pick: false, ban: false };
+			}
+			/* spec */
+			head.push({id: sid, name:s.name ,image: s.image, role: s.role, portrait:s.portrait, clazz: spec_clazz, clazz_repo: head_clazz_repo });
+		}
+		/* job */
+		phead.push({id: id, name:c.name, image: c.image, nos:specz.length, clazz: job_clazz, clazz_repo: phead_clazz_repo }); //nos - the number of speicalizations in a class
+	}
+	return {
+		phead	: phead,
+		head	: head,
+		body	: body
+	};
+};
+
+var renderHead = function(phead, head) {
+	var $phead = jQuery("#ban-spec-list .clazz-list .clazz-parent");
+	var $head = jQuery("#ban-spec-list .clazz-list .clazz-child");
+
+	var spczTotCnt = head.length;
+
+	var pheadArr = [];
+	for(var i=0; i<phead.length; i++) {
+		var ph = phead[i];
+		var spczCnt = ph.nos;
+
+		var width = "100%*"+spczCnt+"/"+spczTotCnt;
+
+		var pdiv = document.createElement("div");
+		pdiv.className = "item-box "+ph.clazz;
+		pdiv.style.width = "calc("+width+")";
+		pdiv.clazz_repo = ph.clazz_repo;
+		pdiv.clazz_key = ph.clazz;
+
+			var img = document.createElement("img");
+			img.src = TALENT_IMG_PREFIX+ph.image;
+			var label = document.createElement("div");
+			label.innerHTML = ph.name;
+
+		pdiv.appendChild(img);
+		pdiv.appendChild(label);
+		pheadArr.push(pdiv);
+	}
+
+	var headArr = [];
+	for(var i=0; i<head.length; i++) {
+		var h = head[i];
+
+		var width = "100%/"+spczTotCnt;
+
+		var div = document.createElement("div");
+		div.className = "item-box "+h.clazz;
+		div.style.width = "calc("+width+")";
+		div.clazz_repo = h.clazz_repo;
+		div.clazz_key = h.clazz;
+
+			var img = document.createElement("img");
+			img.src = TALENT_IMG_PREFIX+h.image;
+			img.title = h.name;
+			//var label = document.createElement("div");
+			//label.innerHTML = ph.name;
+
+		div.appendChild(img);
+		//pdiv.appendChild(label);
+		headArr.push(div);
+	}
+
+	$phead.append(pheadArr);
+	$head.append(headArr);
+};
+
+var renderLeftBody = function(covs) {
+	var $left = jQuery("#ban-spec-list .covenants-list");
+
+	var covArr = [];
+	for(var c in covs) {
+		var cv = covs[c];
+		//var width = "100%/"+spczTotCnt;
+
+		var div = document.createElement("div");
+		div.className = "item-box "+cv.clazz;
+		div.clazz_repo = ["."+cv.clazz];
+		div.clazz_key = cv.clazz;
+		//div.style.width = "calc("+width+")";
+
+			var img = document.createElement("img");
+			img.src = COVENANT_IMG_PREFIX+cv.image;
+			img.title = cv.name;
+			var label = document.createElement("span");
+			label.innerHTML = cv.name;
+
+		div.appendChild(img);
+		div.appendChild(label);
+		covArr.push(div);
+	}
+
+	$left.append(covArr);
+};
+
+var renderRightBody = function(covs, head) {
+	var $right = jQuery("#ban-spec-list .spec-map");
+
+	var bodyArr = [];
+	for(var c in covs) {
+		var cov_clazz = covs[c]["clazz"];
+		var row = document.createElement("div");
+		row.className = "row-container";
+		row.style.marginBottom="6px";
+		for(var i=0; i<head.length; i++) {
+			var h = head[i];
+
+			var div = document.createElement("div");
+			div.className = "item-box "+h.clazz+" "+cov_clazz;
+			div.draggable = true;
+			div.clazz_repo = h.clazz_repo.concat("."+cov_clazz);
+			div.id = h.id+"-"+c;
+			//div["data-portrait"] = h.portrait;
+			div["data-wow"] = new WowSpec(h, covs[c]);
+			row.appendChild(div);
+		}
+		bodyArr.push(row);
+	}
+	$right.append(bodyArr);
+};
+
+var ImageSlider = function(keys, mapData) {
+	var _arr = keys;
+	var _idx = -1;
+	var MAX_IDX = _arr.length -1;
+	var MIN_IDX = 0;
+
+	this.getNext = function() {
+		_idx ++;
+
+		if(_idx > MAX_IDX) _idx = MIN_IDX;
+
+		return mapData[_arr[_idx]];
+	};
+
+	this.getPrev = function() {
+		_idx --;
+
+		if(_idx < MIN_IDX) _idx = MAX_IDX;
+
+		return mapData[_arr[_idx]];
+	};
+};
+
+var Timer = function(seconds, callback) {
+	//var _mm = Math.floor(seconds/60);
+	//var _ss = seconds % 60;
+	var _starting = seconds;
+	var _seconds = seconds;
+	var _callback = callback;
+
+	this.init = function() {
+		this.reset();
+		_callback(_seconds);
+	};
+
+	this.tick = function() {
+		_seconds--;
+		_callback(_seconds);
+	};
+
+	this.reset = function() {
+		_seconds = _starting;
+	};
+};
---
+++ js/data.js
@@ -0,0 +1,161 @@
+var ins_data = {
+	"otherside"	: { image: "de-other-side.jpg", name: "저-편" },
+	"atonement"	: { image: "halls-of-atonement.jpg", name: "속죄의 전당" },
+	"tirna"			: { image: "mists-of-tirna-scithe.jpg", name: "티르너사이드의 안개" },
+	"plague"		: { image: "plaguefall.jpg", name: "역병 몰락지" },
+	"sanguine"	: { image: "sanguine-depths.jpg", name: "핏빛 심연" },
+	"ascension"	: { image: "spires-of-ascension.jpg", name: "승천의 첨탑" },
+	"theater"		: { image: "theater-of-pain.jpg", name: "고통의 투기장" },
+	"necrotic"	: { image: "the-necrotic-wake.jpg", name: "죽음의 상흔" }
+};
+var cov_data = {
+	"kyrian"		: { image: "ui_sigil_kyrian.jpg", "name": "키리안", clazz: "cov-kyrian" },
+	"necrolord"	: { image: "ui_sigil_necrolord.jpg", "name": "강령술사", clazz: "cov-necrolord" },
+	"nightfae"	: { image: "ui_sigil_nightfae.jpg", "name": "나이트페이", clazz: "cov-nightfae" },
+	"venthyr"		: { image: "ui_sigil_venthyr.jpg", "name": "벤티트", clazz: "cov-venthyr" }
+};
+var cls_data = {
+	"death_knight"	: {
+		image: "death_knight/죽음의기사.png",
+		name: "죽기",
+		clazz: "job-death_knight",
+		specializations: [
+      { image: "death_knight/혈기.jpg", role: "T", name: "혈죽", clazz: "spec-death_knight-1", portrait: "death_knight/classes-death-knight-blood.jpg" },
+			{ image: "death_knight/냉기.jpg", role: "D", name: "냉죽", clazz: "spec-death_knight-2", portrait: "death_knight/classes-death-knight-frost.jpg" },
+			{ image: "death_knight/부정.jpg", role: "D", name: "부죽", clazz: "spec-death_knight-3", portrait: "death_knight/classes-death-knight-unholy.jpg" }
+		]
+	},
+	"demon_hunter"	: {
+		image: "demon_hunter/악마사냥꾼.png",
+		name: "악사",
+		clazz: "job-demon_hunter",
+		specializations: [
+			{ image: "demon_hunter/복수.jpg", role: "T", name: "악탱", clazz: "spec-demon_hunter-1", portrait: "demon_hunter/classes-demon-hunter-vengeance.jpg" },
+			{ image: "demon_hunter/파멸.jpg", role: "D", name: "악딜", clazz: "spec-demon_hunter-2", portrait: "demon_hunter/classes-demon-hunter-havoc.jpg" }
+		]
+	},
+	"druid"	: {
+		image: "druid/드루이드.png",
+		name: "드루",
+		clazz: "job-druid",
+		specializations: [
+			{ image: "druid/수호.jpg", role: "T", name: "수드", clazz: "spec-druid-1", portrait: "druid/classes-druid-guardian.jpg" },
+			{ image: "druid/야성.jpg", role: "D", name: "야드", clazz: "spec-druid-2", portrait: "druid/classes-druid-feral.jpg" },
+			{ image: "druid/조화.jpg", role: "D", name: "조드", clazz: "spec-druid-3", portrait: "druid/classes-druid-balance.jpg" },
+			{ image: "druid/회복.jpg", role: "H", name: "회드", clazz: "spec-druid-4", portrait: "druid/classes-druid-restoration.jpg" }
+		]
+	},
+	"hunter"	: {
+		image: "hunter/사냥꾼.png",
+		name: "사냥꾼",
+		clazz: "job-hunter",
+		specializations: [
+			{ image: "hunter/사격.jpg", role: "D", name: "격냥", clazz: "spec-hunter-1", portrait: "hunter/classes-hunter-marksmanship.jpg" },
+			{ image: "hunter/생존.jpg", role: "D", name: "생냥", clazz: "spec-hunter-2", portrait: "hunter/classes-hunter-survival.jpg" },
+			{ image: "hunter/야수.jpg", role: "D", name: "야냥", clazz: "spec-hunter-3", portrait: "hunter/classes-hunter-beast-mastery.jpg" }
+		]
+	},
+	"mage"	: {
+		image: "mage/마법사.png",
+		name: "마법사",
+		clazz: "job-mage",
+		specializations: [
+			{ image: "mage/냉기.jpg", role: "D", name: "냉법", clazz: "spec-mage-1", portrait: "mage/classes-mage-frost.jpg" },
+			{ image: "mage/비전.jpg", role: "D", name: "비법", clazz: "spec-mage-2", portrait: "mage/classes-mage-arcane.jpg" },
+			{ image: "mage/화염.jpg", role: "D", name: "화법", clazz: "spec-mage-3", portrait: "mage/classes-mage-fire.jpg" }
+		]
+	},
+	"monk"	: {
+		image: "monk/수도사.png",
+		name: "수도사",
+		clazz: "job-monk",
+		specializations: [
+			{ image: "monk/양조.jpg", role: "T", name: "양조", clazz: "spec-monk-1", portrait: "monk/classes-monk-brewmaster.jpg" },
+			{ image: "monk/풍운.jpg", role: "D", name: "풍운", clazz: "spec-monk-2", portrait: "monk/classes-monk-windwalker.jpg" },
+      { image: "monk/운무.jpg", role: "H", name: "운무", clazz: "spec-monk-3", portrait: "monk/classes-monk-mistweaver.jpg" }
+		]
+	},
+	"paladin"	: {
+		image: "paladin/성기사.png",
+		name: "성기사",
+		clazz: "job-paladin",
+		specializations: [
+			{ image: "paladin/보호.jpg", role: "T", name: "보기", clazz: "spec-paladin-1", portrait: "paladin/classes-paladin-protection.jpg" },
+			{ image: "paladin/징벌.jpg", role: "D", name: "징기", clazz: "spec-paladin-2", portrait: "paladin/classes-paladin-retribution.jpg" },
+      { image: "paladin/신성.jpg", role: "H", name: "신기", clazz: "spec-paladin-3", portrait: "paladin/classes-paladin-holy.jpg" }
+		]
+	},
+	"priest"	: {
+		image: "priest/사제.png",
+		name: "사제",
+		clazz: "job-priest",
+		specializations: [
+      { image: "priest/암흑.jpg", role: "D", name: "암사", clazz: "spec-priest-1", portrait: "priest/classes-priest-shadow.jpg" },
+			{ image: "priest/수양.jpg", role: "H", name: "수사", clazz: "spec-priest-2", portrait: "priest/classes-priest-discipline.jpg" },
+			{ image: "priest/신성.jpg", role: "H", name: "신사", clazz: "spec-priest-3", portrait: "priest/classes-priest-holy.jpg" }
+		]
+	},
+	"rogue"	: {
+		image: "rogue/도적.png",
+		name: "도적",
+		clazz: "job-rogue",
+		specializations: [
+			{ image: "rogue/무법.jpg", role: "D", name: "무법", clazz: "spec-rogue-1", portrait: "rogue/classes-rogue-outlaw.jpg" },
+			{ image: "rogue/암살.jpg", role: "D", name: "암살", clazz: "spec-rogue-2", portrait: "rogue/classes-rogue-assassination.jpg" },
+			{ image: "rogue/잠행.jpg", role: "D", name: "잠행", clazz: "spec-rogue-3", portrait: "rogue/classes-rogue-subtlety.jpg" }
+		]
+	},
+	"shaman"	: {
+		image: "shaman/주술사.png",
+		name: "주술사",
+		clazz: "job-shaman",
+		specializations: [
+			{ image: "shaman/고양.jpg", role: "D", name: "고술", clazz: "spec-shaman-1", portrait: "shaman/classes-shaman-enhancement.jpg" },
+			{ image: "shaman/정기.jpg", role: "D", name: "정술", clazz: "spec-shaman-2", portrait: "shaman/classes-shaman-elemental.jpg" },
+			{ image: "shaman/복원.jpg", role: "H", name: "복술", clazz: "spec-shaman-3", portrait: "shaman/classes-shaman-restoration.jpg" }
+		]
+	},
+	"warlock"	: {
+		image: "warlock/흑마법사.png",
+		name: "흑마",
+		clazz: "job-warlock",
+		specializations: [
+			{ image: "warlock/고통.jpg", role: "D", name: "고흑", clazz: "spec-warlock-1", portrait: "warlock/classes-warlock-affliction.jpg" },
+			{ image: "warlock/악마.jpg", role: "D", name: "악흑", clazz: "spec-warlock-2", portrait: "warlock/classes-warlock-demonology.jpg" },
+			{ image: "warlock/파괴.jpg", role: "D", name: "파흑", clazz: "spec-warlock-3", portrait: "warlock/classes-warlock-destruction.jpg" }
+		]
+	},
+	"worrior"	: {
+		image: "worrior/전사.png",
+		name: "전사",
+		clazz: "job-worrior",
+		specializations: [
+			{ image: "worrior/방어.jpg", role: "T", name: "방전", clazz: "spec-worrior-1", portrait: "worrior/classes-warrior-protection.jpg" },
+			{ image: "worrior/무기.jpg", role: "D", name: "무전", clazz: "spec-worrior-2", portrait: "worrior/classes-warrior-arms.jpg" },
+			{ image: "worrior/분노.jpg", role: "D", name: "분전", clazz: "spec-worrior-3", portrait: "worrior/classes-warrior-fury.jpg" }
+		]
+	}
+};
+
+var dropSequences = [
+  { id: "team-A-ban-1", desc: "탱커밴(선공)" },
+  { id: "team-B-ban-1", desc: "탱커밴(후공)" },
+  { id: "team-A-pick-1", desc: "탱커픽(선공)" },
+  { id: "team-B-pick-1", desc: "탱커픽(후공)" },
+  { id: "team-B-ban-2", desc: "딜러밴1(후공)" },
+  { id: "team-A-ban-2", desc: "딜러밴1(선공)" },
+  { id: "team-A-ban-3", desc: "딜러밴2(선공)" },
+  { id: "team-B-ban-3", desc: "딜러밴2(후공)" },
+  { id: "team-B-ban-4", desc: "딜러밴3(후공)" },
+  { id: "team-A-ban-4", desc: "딜러밴3(선공)" },
+  { id: "team-B-pick-2", desc: "딜러픽1(후공)" },
+  { id: "team-A-pick-2", desc: "딜러픽1(선공)" },
+  { id: "team-A-pick-3", desc: "딜러픽2(선공)" },
+  { id: "team-B-pick-3", desc: "딜러픽2(후공)" },
+  { id: "team-B-pick-4", desc: "딜러픽3(후공)" },
+  { id: "team-A-pick-4", desc: "딜러픽3(선공)" },
+  { id: "team-B-pick-5", desc: "힐러밴(후공)" },
+  { id: "team-A-ban-5", desc: "힐러밴(선공)" },
+  { id: "team-B-pick-5", desc: "힐러픽(후공)" },
+  { id: "team-A-ban-5", desc: "힐러픽(선공)" }
+];
